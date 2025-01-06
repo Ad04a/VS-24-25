bf0291a37dc685932fe8c0d1c3b67dc083d8f43e
Ad04a <andrey.todorov04@gmail.com>
2025-01-06 08:54:04 +0200

    try

--- Classwork/06-01-25.c
+++ Classwork/06-01-25.c
@@ -2,13 +2,10 @@
 
 int main()
 {
-    int n;
-
-    scanf("%d", &n);
-
+    int n=7;
     switch(n)
     {
-        case 3: printf("= 3 =");break;
+        case 3: ;break;
         case 4: printf(" = 4 = ");break;
         default: pritnf(" no case ");
     }
