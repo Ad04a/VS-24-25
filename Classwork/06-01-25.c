25e1bc6c550caa4fa5ae6a33062a20d7ed1cb2a2
Ad04a <andrey.todorov04@gmail.com>
2025-01-06 08:31:09 +0200

    slushalki

---
+++ Classwork/06-01-25.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    if(n == 3)
+    {
+        printf(" - 3 - ");
+    } else if(n == 4)
+    {
+        printf(" -  4  -");
+    }
+}
